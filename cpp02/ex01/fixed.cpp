00ca846d0e51921a831a72decbf89d29939114c8
andres <andres.stefanescu@gmail.com>
2026-01-03 14:14:53 +0100

    arreglos en ex00 y 01

--- cpp02/ex01/fixed.cpp
+++ cpp02/ex01/fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: astefane <astefane@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 15:17:32 by astefane          #+#    #+#             */
-/*   Updated: 2025/12/29 14:28:14 by astefane         ###   ########.fr       */
+/*   Updated: 2026/01/03 14:11:10 by astefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ fixed& fixed::operator=(const fixed& other)
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
         _rawBits = other._rawBits;
-    return *this;
+    return (*this);
 }
 
 /* Destructor */
@@ -55,7 +55,7 @@ fixed::~fixed()
 /* Getters / setters */
 int fixed::getRawBits(void) const
 {
-    return _rawBits;
+    return (_rawBits);
 }
 
 void fixed::setRawBits(int const raw)
@@ -66,17 +66,17 @@ void fixed::setRawBits(int const raw)
 /* Conversions */
 float fixed::toFloat(void) const
 {
-    return (float)_rawBits / (1 << _fractionalBits);
+    return ((float)_rawBits / (1 << _fractionalBits));
 }
 
 int fixed::toInt(void) const
 {
-    return _rawBits >> _fractionalBits;
+    return (_rawBits >> _fractionalBits);
 }
 
 /* Operator << */
 std::ostream& operator<<(std::ostream& out, const fixed& value)
 {
     out << value.toFloat();
-    return out;
+    return (out);
 }
