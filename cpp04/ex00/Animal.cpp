69a91a06f2f7b0ad7d3b206bba3ec259111e88e2
andrestefane <andres.stefanescu@gmail.com>
2026-01-08 04:52:42 +0100

    cpp04 ex00

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,22 @@
+
+#include "Animal.hpp"
+
+Animal::Animal() : type ("Animal")
+{
+    std::cout << "Animal Constructor called" << std::endl;
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal Destructor called" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+    return (type);
+}
+
+void    Animal::makeSound() const
+{
+    std::cout << " Animal Sound " << std::endl;
+}
---
+++ cpp04/ex00/Cat.cpp
@@ -0,0 +1,18 @@
+
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+    type = "Cat";
+    std::cout << "Cat constructor called" << std::endl;
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+void    Cat::makeSound() const
+{
+    std::cout << "Guau" << std::endl;
+}
